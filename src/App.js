1cae2409f628ce0c98bdc05c6af3b57e9d52a8b0
EmilioJasso19 <emiliojasso0409@gmail.com>
2023-08-24 02:46:40 -0600

    Se cambiaron los equipos y colaboradores

--- src/App.js
+++ src/App.js
@@ -15,99 +15,102 @@ function App() {
         {
             id: uuid(),
             nombre: 'Emilio Jasso',
-            puesto: 'Programador',
+            puesto: 'Personita',
             foto: 'https://avatars.githubusercontent.com/u/123335629?s=96&v=4',
-            equipo: 'Front End',
+            equipo: 'Personas normales',
             fav: true
         },
         {
 
             id: uuid(),
-            nombre: "Harland Lohora",
-            puesto: "Instructor",
-            foto: "https://github.com/harlandlohora.png",
-            equipo: "Front End",
+            nombre: "Tony Stark",
+            puesto: "Iron-Man",
+            foto: "https://i.pinimg.com/originals/40/cc/95/40cc95f21c4596a28e833ff7527b77cb.png",
+            equipo: "Los Vengadores",
             fav: false
         },
         {
 
             id: uuid(),
-            nombre: "Genesys Rondon",
-            puesto: "Desarrolladora de software e instructora",
-            foto: "https://github.com/genesysrm.png",
-            equipo: "Programacion",
+            nombre: "Steve Rogers",
+            puesto: "Capitan America",
+            foto: "https://i.pinimg.com/474x/32/11/ef/3211efbab4dd5ee7a0a9ea561494a1ea.jpg",
+            equipo: "Los Vengadores",
             fav: false
         },
         {
 
             id: uuid(),
-            nombre: "Jeanmarie Quijada",
-            puesto: "Instructora en Alura Latam",
-            foto: "https://github.com/JeanmarieAluraLatam.png",
-            equipo: "UX y Diseño",
+            nombre: "Charles Xavier",
+            puesto: "Profesor X",
+            foto: "https://i.pinimg.com/564x/5d/f8/05/5df8058334236eefb3675947cfdef118.jpg",
+            equipo: "X-Men",
             fav: false
         },
         {
 
             id: uuid(),
-            nombre: "Christian Velasco",
-            puesto: "Head de Alura e Instructor",
-            foto: "https://github.com/christianpva.png",
-            equipo: "Programacion",
+            nombre: "Reed Richards",
+            puesto: "Mr. Fantastic",
+            foto: "https://i.pinimg.com/736x/05/3f/0e/053f0ee86947f87c15341c6f3f22b494.jpg",
+            equipo: "Los 4 Fantasticos",
             fav: false
         },
         {
 
             id: uuid(),
-            nombre: "Jose Gonzalez",
-            puesto: "Dev FullStack",
-            foto: "https://github.com/JoseDarioGonzalezCha.png",
-            equipo: "Innovacion y Gestión",
+            nombre: "Wade Wilson",
+            puesto: "Deadpool",
+            foto: "https://i.pinimg.com/550x/ae/ea/7c/aeea7c61fd0e44de66db8d12a6287e38.jpg",
+            equipo: "X-Force",
+            fav: false
+        },
+        {
+
+            id: uuid(),
+            nombre: "Peter Parker",
+            puesto: "Spider-Man",
+            foto: "https://cdn-icons-png.flaticon.com/256/1090/1090806.png",
+            equipo: "Vecindario",
             fav: false
         }])
 
     const [ equipos, actualizarEquipos ] = useState([
         {
             id: uuid(),
-            titulo: 'Programacion',
+            titulo: 'Los Vengadores',
             colorPrimario: '#57C278',
             colorSecundario: '#D9F7E9'
         },
         {
             id: uuid(),
-            titulo: 'Front End',
+            titulo: 'Los 4 Fantasticos',
             colorPrimario: '#82CFFA',
             colorSecundario: '#E8F8FF'
         },
         {
             id: uuid(),
-            titulo: 'Data Science',
+            titulo: 'X-Men',
             colorPrimario: '#A6D157',
             colorSecundario: '#F0F8E2'
         },
         {
             id: uuid(),
-            titulo: 'Devops',
+            titulo: 'X-Force',
             colorPrimario: '#E06B69',
             colorSecundario: '#FDE7E8'
         },
         {
             id: uuid(),
-            titulo: 'UX y Diseño',
+            titulo: 'Vecindario',
             colorPrimario: '#DB6EBF',
             colorSecundario: '#FAE9F5'
         },
         {
             id: uuid(),
-            titulo: 'Movil',
+            titulo: 'Personas normales',
             colorPrimario: '#FFBA05',
             colorSecundario: '#FFF5D9'
-        },
-        {
-            id: uuid(),
-            titulo: 'Innovacion y Gestión',
-            colorPrimario: '#FF8A29',
-            colorSecundario: '#FFEEDF'
         }
         ])
 
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,18 +2,18 @@ import "./Footer.css";
 const Footer = ()=>{
     return <footer className="footer" style={{backgroundImage:"url(/img/Footer.png)"}}>
         <div className="redes">
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.facebook.com/millo.jaslo.5/' target='blank'>
                 <img src="/img/facebook.png" alt="Facebook"/>
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://twitter.com/EmilioJasso14' target='blank'>
                 <img src="/img/twitter.png" alt="twitter" />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.instagram.com/millojaslo/' target='blank'>
                 <img src="/img/instagram.png" alt="instagram" />
             </a>
         </div>
         <img src='/img/Logo.png' alt='org' />
-        <strong>Desarrollado por Alura</strong>
+        <strong>Desarrollado por Emilio Jasso</strong>
     </footer>
 }
 export default Footer;
